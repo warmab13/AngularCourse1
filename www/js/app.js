da58c795dc50f95afe0d9364d260461f04aff37d
Administrador <administrador@lc_012.itchii.lab>
2016-08-30 12:56:11 -0600

    First Controller, and angular definition

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,8 @@
 (function(){
-	var gem = { 
-		name: 'Azurite', 
-		price: 2.95, 
-		description:'Lorem ipsum dolor sit amet'
-	};
-	
+
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+
 	});
 })();
-	
