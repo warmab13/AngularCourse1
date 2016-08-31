d1c08eed685ae1e2f036ea414d0397b4bc3eaf6d
warmab13 <alonsosalcido13@gmail.com>
2016-08-31 12:57:43 -0600

    Directives, nghide, ng-show, ngrepeat

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,40 @@
 (function(){
 
+	var gem = {
+		name: 'Azurite',
+		price: '2.95',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+		canPurchase: false,
+		soldOut: true
+	};
+
+	var gems = [
+		{
+			name: 'Azurite',
+			price: '2.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true
+		},
+		{
+			name: 'Bloodstone',
+			price: '5.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true
+		},
+		{
+			name: 'Zircon',
+			price: '3.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true
+		}	
+	];
+
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function(){
-
+		this.products = gems;
 	});
 })();
