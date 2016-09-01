3998c751d7390d936a766bd5654f057792ac94d3
warmab13 <alonsosalcido13@gmail.com>
2016-09-01 12:55:26 -0600

    Ngsource, tabs

--- www/js/app.js
+++ www/js/app.js
@@ -14,21 +14,36 @@
 			price: '2.95',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
 			canPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-01.gif'},
+				{id:1, url:'img/gem-02.gif'},
+				{id:2, url:'img/gem-03.gif'}
+			]
 		},
 		{
 			name: 'Bloodstone',
 			price: '5.95',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
 			canPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-04.gif'},
+				{id:1, url:'img/gem-05.gif'},
+				{id:2, url:'img/gem-06.gif'}
+			]
 		},
 		{
 			name: 'Zircon',
 			price: '3.95',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
 			canPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-07.gif'},
+				{id:1, url:'img/gem-08.gif'},
+				{id:2, url:'img/gem-09.gif'}
+			]
 		}	
 	];
 
