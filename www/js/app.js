6769295b6af0de8994256af526990e31e259aefb
warmab13 <alonsosalcido13@gmail.com>
2016-09-05 12:51:50 -0600

    Tabs, PanelController Error

--- www/js/app.js
+++ www/js/app.js
@@ -52,4 +52,17 @@
 	app.controller('StoreController', function(){
 		this.products = gems;
 	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
 })();
