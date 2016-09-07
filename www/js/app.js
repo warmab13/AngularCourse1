d9c7fb11006239fdb3fddbfdea7bb1168fff495b
warmab13 <alonsosalcido13@gmail.com>
2016-09-07 12:54:37 -0600

    Reviews add, form valid and submit

--- www/js/app.js
+++ www/js/app.js
@@ -79,6 +79,11 @@
 
 	app.controller('ReviewsController',function(){
       this.review = {};
+      this.addReview = function(product){
+      	product.reviews.push(this.review);
+      	this.review = {};
+      }
+
 	});
 
 })();
