8e99a41750d6322c8d32a9144dbb66f7a239ab8d
warmab13 <alonsosalcido13@gmail.com>
2016-09-12 12:55:04 -0600

    Custom directives, tabs, reviews

--- www/js/app.js
+++ www/js/app.js
@@ -100,5 +100,40 @@
 		};
 	});
 
+	app.directive('productPanels', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function(){
+				this.tab = 1;
+
+				this.selectTab = function(setTab){
+					this.tab = setTab;
+				};
+
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				};
+			},
+			controllerAs: 'panel'
+		}
+	});
+
+	app.directive('reviewsDirective', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'reviews.html',
+			controller: function(){
+				this.review = {};
+			    this.addReview = function(product){
+			      	product.reviews.push(this.review);
+			      	this.review = {};
+			    }
+			},
+			controllerAs: 'reviewCtrl'
+		}
+	})
+
+
 
 })();
